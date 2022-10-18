b06dc2e532dce9e9563fe3b5f3e17ca4f1a36680
Stephen Bacso <stephen.bacso@gmail.com>
2022-10-18 13:50:44 -0400

    start

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { mainNavbarItems } from "./consts/navbarItems";
+import { navbarStyles } from "./styles";
+
+const Navbar = () => {
+  return (
+    <Drawer sx={navbarStyles.drawer} variant="permanent" anchor="left">
+      <Toolbar />
+      <Divider />
+      <List>
+        {mainNavbarItems.map((item, index) => (
+          <ListItem button key={item.id}>
+            <ListItemIcon sx={navbarStyles.icons}>{item.icon}</ListItemIcon>
+            <ListItemText sx={navbarStyles.text} primary={item.label} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default Navbar;
